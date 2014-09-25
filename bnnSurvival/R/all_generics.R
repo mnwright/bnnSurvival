7da6fd54480a56bf7f3208190c006604a4b6c6cc
Marvin Wright <wright@imbs.uni-luebeck.de>
2014-09-25 16:52:57 +0200

    readd predictSurvProb method

--- bnnSurvival/R/all_generics.R
+++ bnnSurvival/R/all_generics.R
@@ -8,3 +8,19 @@ setGeneric("predictions", function(object, ...) standardGeneric("predictions"))
 ##' @param object Object to extract timepoints from
 ##' @param ... further arguments passed to or from other methods.
 setGeneric("timepoints", function(object, ...) standardGeneric("timepoints"))
+
+##' Function to extract survival probability predictions from bnnSurvivalEnsemble. Use with \code{pec} package.
+##' @param object bnnSurvivalEnsemble object.
+##' @param newdata Data used for prediction.
+##' @param times Not used.
+##' @param ... Not used.
+##' @return survival probability predictions
+##' @export
+predictSurvProb.bnnSurvivalEnsemble <- function(object, newdata, times, ...) {
+  pred <- predict(object, newdata)
+  pos <- sapply(times, function(x) {
+    max(c(0, which(x >= timepoints(pred))))
+  })    
+  p <- cbind(1, predictions(pred))[, pos+1, drop = FALSE]
+  return(p)
+}
