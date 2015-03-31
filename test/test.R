0fa96a412a7b9129d22e09c7a860385da7a08359
Marvin Wright <wright@imbs.uni-luebeck.de>
2015-03-31 10:58:20 +0200

    add new release version

--- test/test.R
+++ test/test.R
@@ -30,7 +30,7 @@ model <- bnnSurvival(formula, train_data, replace = FALSE, sample_fraction = 1)
 predict(model, test_data)
 
 ## Test optimal k 
-bnnSurvival(Surv(time, status) ~ ., veteran, k = c(5,10,20), num_base_learners = 1)
+bnnSurvival(Surv(time, status) ~ ., veteran, k = c(5,10,20, 25, 50, 100), num_base_learners = 1)
 sapply(1:6, function(x) {
   bnnSurvival(Surv(time, status) ~ ., veteran, k = 1:20, num_base_learners = 50, num_features_per_base_learner = x)@k
 })
