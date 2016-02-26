9bdf5ac7c78cbef922f74f2d350a1fa78bdb004a
Marvin Wright <wright@imbs.uni-luebeck.de>
2016-02-26 17:01:51 +0100

    add pec test

---
+++ tests/testthat/test_pec.R
@@ -0,0 +1,20 @@
+library(survival)
+library(bnnSurvival)
+library(pec)
+
+context("pec")
+
+## Data
+dat <- veteran
+n <- nrow(dat)
+idx <- sample(n, 2/3*n)
+train_data <- dat[idx, ]
+test_data <- dat[-idx, ]
+timepoints <- sort(unique(train_data$time))
+formula <- formula(Surv(time, status) ~ age + trt + prior)
+
+test_that("pec predictions works", {
+  bnn <- bnnSurvival(formula, train_data, k = 5, num_base_learners = 3)
+  fitpec <- pec(bnn, formula = formula, data = test_data)
+  expect_that(fitpec, is_a("pec"))
+})
